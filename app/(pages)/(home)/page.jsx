c47b271000e2f3f373a12c65c65eadd18f4a963e
kyhchn <kyaahh69@gmail.com>
2024-03-06 13:50:05 +0700

    feat(landing) : slice portfolio and article

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,4 +1,21 @@
+import * as React from "react";
 import Image from "next/image";
+import { Eye } from "lucide-react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 import {
   DAVIZ_DESC,
   FEATURE_1,
@@ -9,6 +26,7 @@ import {
   FEATURE_6,
   FEATURE_7,
   FEATURE_8,
+  PORTFOLIO,
   THREE_DOT,
   WHAT_IS_DAVIZ,
 } from "@/utils/assets";
@@ -91,16 +109,38 @@ export default function Home() {
       image: FEATURE_8,
     },
   ];
+  const dropdownData = [
+    {
+      title: "Daviz Dashboard Management",
+      content:
+        "Daviz Dashboard Management is an innovative service designed to provide intuitive and interactive data visualization solutions. Using the Daviz platform, we develop dashboards that are not only visually stunning, but also rich in analytical features. This service allows users to see and understand data in a more effective and efficient way.",
+    },
+    {
+      title: "Daviz Dashboard Management",
+      content:
+        "Daviz Dashboard Management is an innovative service designed to provide intuitive and interactive data visualization solutions. Using the Daviz platform, we develop dashboards that are not only visually stunning, but also rich in analytical features. This service allows users to see and understand data in a more effective and efficient way.",
+    },
+    {
+      title: "Daviz Dashboard Management",
+      content:
+        "Daviz Dashboard Management is an innovative service designed to provide intuitive and interactive data visualization solutions. Using the Daviz platform, we develop dashboards that are not only visually stunning, but also rich in analytical features. This service allows users to see and understand data in a more effective and efficient way.",
+    },
+    {
+      title: "Daviz Dashboard Management",
+      content:
+        "Daviz Dashboard Management is an innovative service designed to provide intuitive and interactive data visualization solutions. Using the Daviz platform, we develop dashboards that are not only visually stunning, but also rich in analytical features. This service allows users to see and understand data in a more effective and efficient way.",
+    },
+  ];
   const informationCard = ({ title, desc, growth, value }) => {
     const highlightColor = growth > 0 ? "#47CC45" : "red-500";
     return (
       <div className="flex flex-col rounded-lg bg-white p-4 shadow-md">
         <div className="flex flex-row items-start justify-between">
           <div>
-            <p className="text-daviz-grey-700 mb-2 text-base">{title}</p>
-            <p className="text-daviz-grey-400 text-xs">{desc}</p>
+            <p className="mb-2 text-base text-daviz-grey-700">{title}</p>
+            <p className="text-xs text-daviz-grey-400">{desc}</p>
           </div>
-          <div className="bg-daviz-grey-700 rounded p-2 opacity-5">
+          <div className="rounded bg-daviz-grey-700 p-2 opacity-5">
             <img src={THREE_DOT} alt="threedots" width={16} height={16} />
           </div>
         </div>
@@ -111,7 +151,7 @@ export default function Home() {
               {growth + "%"}
             </p>
           </div>
-          <p className="text-black-base text-lg font-semibold">{dollarFormatter(value)}</p>
+          <p className="text-lg font-semibold text-black-base">{dollarFormatter(value)}</p>
         </div>
       </div>
     );
@@ -129,16 +169,37 @@ export default function Home() {
         <div className="w-[10%] "></div>
         <div className="w-[45%] flex-col justify-start">
           <div className="flex w-full flex-row items-center">
-            <p className="text-daviz-red-500 text-2xl font-bold italic">{"0" + order}.</p>
+            <p className="text-2xl font-bold italic text-daviz-red-500">{"0" + order}.</p>
             <div className="w-3"></div>
-            <p className=" text-black-base text-xl font-medium">{title}</p>
+            <p className=" text-xl font-medium text-black-base">{title}</p>
           </div>
           <div className="w-6"></div>
-          <p className="text-black-base whitespace-pre-wrap text-justify text-base font-normal">
+          <p className="whitespace-pre-wrap text-justify text-base font-normal text-black-base">
             {desc}
           </p>
         </div>
       </div>
+    );
+  };
+
+  const dropDownBuilder = ({ dropdownList }) => {
+    return (
+      <Accordion type="single" collapsible className="w-full">
+        {dropdownList.map((dropdown, index) => (
+          <AccordionItem value={`item-${index + 1}`}>
+            <AccordionTrigger>
+              <div className="flex flex-row items-center justify-center underline-offset-0">
+                <p className="text-lg font-normal text-daviz-grey-400">
+                  {index.toString().padStart(2, "0")}
+                </p>
+                <div className="w-8"></div>
+                <p className="text-2xl font-medium text-black-base">{dropdown.title}</p>
+              </div>
+            </AccordionTrigger>
+            <AccordionContent className="pl-[52px]">{dropdown.content}</AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
     );
   };
   return (
@@ -156,11 +217,11 @@ export default function Home() {
             sizes="100vw"
           />
           <div className="absolute top-12 w-full px-40 text-center">
-            <p className="text-daviz-red-500 mb-4 text-xl font-semibold">Data Driven Decision</p>
-            <p className="text-black-base mb-4 text-4xl font-semibold">
+            <p className="mb-4 text-xl font-semibold text-daviz-red-500">Data Driven Decision</p>
+            <p className="mb-4 text-4xl font-semibold text-black-base">
               Unleash The Power of Data Analytics
             </p>
-            <p className="text-daviz-grey-500 mb-4 text-base font-medium">
+            <p className="mb-4 text-base font-medium text-daviz-grey-500">
               How to Turn Data into the Most Valuable Asset for the <br /> Progress of Your
               Organization or Company?
             </p>
@@ -173,11 +234,11 @@ export default function Home() {
         </div>
         {/* Description */}
         <div className="mt-20 flex flex-col items-center justify-start px-20">
-          <p className="text-black-base text-3xl font-semibold">
+          <p className="text-3xl font-semibold text-black-base">
             What is <span className="text-red-500">Daviz</span>?
           </p>
           <div className="h-12"></div>
-          <p className="text-black-base whitespace-pre-wrap text-center text-lg font-normal">
+          <p className="whitespace-pre-wrap text-center text-lg font-normal text-black-base">
             {DAVIZ_DESC}
           </p>
           <div className="h-16"></div>
@@ -191,7 +252,7 @@ export default function Home() {
         </div>
         {/* Our Feature */}
         <div className="mt-60 flex flex-col items-center justify-center px-28">
-          <div className="text-black-base inline-block w-auto border-b-8 border-red-500 text-center text-3xl font-semibold">
+          <div className="inline-block w-auto border-b-8 border-red-500 text-center text-3xl font-semibold text-black-base">
             Our Feature
           </div>
           <div className="h-32"></div>
@@ -203,6 +264,60 @@ export default function Home() {
             ))}
           </div>
         </div>
+        <div className="h-20"></div>
+        {/*Our Service */}
+        <div className="flex flex-col justify-center">
+          <h2 className="mb-16 text-center text-3xl font-semibold text-black-base">Our Service</h2>
+          {dropDownBuilder({ dropdownList: dropdownData })}
+        </div>
+        {/* Our Portfolio */}
+        <div className="flex w-full flex-col items-center justify-start">
+          <div>
+            <h2 className="text-3xl font-semibold text-black">Our Portfolio</h2>
+            <p className="text-xl font-normal text-daviz-grey-400">
+              Our recent works about data visualization
+            </p>
+          </div>
+          <div className="h-24"></div>
+          <Carousel className="w-full px-24">
+            <CarouselContent className="-ml-12">
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className="basis-1/3 pl-12">
+                  <div className="flex flex-col items-center justify-start">
+                    <Image alt={`item ${index}`} src={PORTFOLIO} width={340} height={240} />
+                    <div className="h-6"></div>
+                    <div className="flex flex-col items-start justify-start gap-3">
+                      <p className="text-base font-normal text-black-base">Health . 202</p>
+                      <p className="text-xl font-medium text-black-base">
+                        Dashboard Covid-19 Case in Indonesia
+                      </p>
+                      <div className="flex flex-row gap-8">
+                        <div className="flex items-center">
+                          <Eye className="text-daviz-grey-400" />
+                          <div className="w-2"></div>
+                          <p className="text-base font-normal text-daviz-grey-400">141</p>
+                        </div>
+                        <div className="flex items-center">
+                          <Eye className="text-daviz-grey-400" />
+                          <div className="w-2"></div>
+                          <p className="text-base font-normal text-daviz-grey-400">141</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+          <div className="h-20"></div>
+          <Button>
+            <p className="text-base font-medium text-white">View More</p>
+          </Button>
+        </div>
+        {/* Contact Us */}
+        <div className="my-44 flex flex-row rounded-xl"></div>
       </div>
       <div className="h-12"></div>
     </main>
