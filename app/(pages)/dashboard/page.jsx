adc92b76d33b1f9fd48e1ed59d35d97aef8a8cb5
kyhchn <kyaahh69@gmail.com>
2024-03-07 13:14:31 +0700

    fix(portfolio): fix dynamic import

--- app/(pages)/dashboard/page.jsx
+++ app/(pages)/dashboard/page.jsx
@@ -1,19 +1,22 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { TableauViz } from "@tableau/embedding-api";
 
 export default function Page() {
+  async function fn() {
+    const { TableauViz } = await import("@tableau/embedding-api");
+    let viz = new TableauViz();
+    console.log("cek");
+
+    viz.src = "https://public.tableau.com/views/City_AQ_Viewer_2023/EuropeanCityRanking";
+    viz.toolbar = "hidden";
+
+    document.getElementById("tableauViz").appendChild(viz);
+  }
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      const viz = new TableauViz();
-
-      viz.src = "https://public.tableau.com/views/City_AQ_Viewer_2023/EuropeanCityRanking";
-      viz.toolbar = "hidden";
-
-      document.getElementById("tableauViz").appendChild(viz);
-    }
+    fn();
   }, []);
+
   return (
     <main>
       <div className="w-full p-16" id="tableauViz"></div>
